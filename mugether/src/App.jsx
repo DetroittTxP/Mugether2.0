da1f0ba7771d78f67ea4882b017c04f4727de2b1
Parame Srisomsap <s6304062616170@email.kmutnb.ac.th>
2023-10-01 15:57:37 +0700

    Navbar

--- mugether/src/App.jsx
+++ mugether/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from './components/Header'
-
+import './App.css'
 export default function App() {
   return (
     <>
--- mugether/src/components/Header.jsx
+++ mugether/src/components/Header.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import { Container, Nav, Navbar, Form } from 'react-bootstrap'
+import Logo from '../assets/Pap.jpg'
+import {} from 'react-icons'
 export default function Header() {
   return (
-    <header>
-        
-    </header>
+    <div >
+      <Navbar style={{ padding: 30 }} bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={Logo} height={80} width={100} />
+          </Navbar.Brand>
+
+
+          <Nav style={{ margin: auto }} className="me-auto">
+            <Form.Control type="text" placeholder="Search here" style={{ width: 600 }} />
+          </Nav>
+
+
+          <Nav style={{textAlign:'end'}}>
+               
+          </Nav>
+
+        </Container>
+      </Navbar>
+    </div>
   )
 }
