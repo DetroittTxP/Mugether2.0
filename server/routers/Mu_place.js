c23ecd1b359c09a347b4e0341f0a5df7b22e8f08
Parame Srisomsap <s6304062616170@email.kmutnb.ac.th>
2023-10-01 14:34:55 +0700

    Update Mu_place.js

--- server/routers/Mu_place.js
+++ server/routers/Mu_place.js
@@ -1,16 +1,16 @@
 const router = require('express').Router();
 const {MongoClient} = require('mongodb')
+require('dotenv').config({path:'../.env'})
 
 
+const client = new MongoClient(process.env.CONNECT_STRING_BANK)
 
-
-
-router.get('/mudata',(req,res) => {
-
+router.get('/mudata',async (req,res) => {
+     
+       await client.connect();
+       let result = await client.db(process.env.DATABASE).collection(process.env.MU_PLACE)
+       
 })
-
-
-
 
 
 router.get('/',(req,res) => {
