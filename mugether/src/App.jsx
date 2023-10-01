804d8887e533af9c07e8f7cfef4aa2e597d8472d
Parame Srisomsap <s6304062616170@email.kmutnb.ac.th>
2023-10-01 15:15:28 +0700

    ADD HEADER

--- mugether/src/App.jsx
+++ mugether/src/App.jsx
@@ -5,6 +5,8 @@ export default function App() {
   return (
     <>
        <Header/>
+
+      
     </>
   )
 }
--- mugether/src/components/Header.jsx
+++ mugether/src/components/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <header>
+        
+    </header>
   )
 }
