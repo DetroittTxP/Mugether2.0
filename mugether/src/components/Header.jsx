afe5be8f6ecf7bf2ecc048951ed47cb3cfd08647
Parame Srisomsap <s6304062616170@email.kmutnb.ac.th>
2023-10-01 16:29:27 +0700

    HEADER

--- mugether/src/components/Header.jsx
+++ mugether/src/components/Header.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
-import { Container, Nav, Navbar, Form } from 'react-bootstrap'
+import { Container, Nav, Navbar, Form} from 'react-bootstrap'
+import { Button, Dropdown } from 'antd';
 import Logo from '../assets/Pap.jpg'
-import {} from 'react-icons'
+import { auto } from '@popperjs/core'
+import {SlLogin} from 'react-icons/sl'
+
 export default function Header() {
+
+  const items = [
+    {
+      key: '1',
+      label: (
+        <h5>test</h5>
+      ),
+    },
+   
+  ];
+
+
   return (
     <div >
       <Navbar style={{ padding: 30 }} bg="light" data-bs-theme="light">
         <Container>
           <Navbar.Brand href="#home">
-            <img src={Logo} height={80} width={100} />
+            <img src={Logo} height={80} width={100} style={{borderRadius:50}} />
           </Navbar.Brand>
 
 
@@ -16,13 +31,21 @@ export default function Header() {
             <Form.Control type="text" placeholder="Search here" style={{ width: 600 }} />
           </Nav>
 
+      
+                 <Dropdown menu={{items,}} >
+                      <Button style={{width:100,height:50,textAlign:'center',borderRadius:50}}>
+                        
+                             <SlLogin style={{marginLeft:50,fontSize:'larger'}}/>
 
-          <Nav style={{textAlign:'end'}}>
-               
-          </Nav>
+                      </Button>
+                 </Dropdown>
+      
+           
 
         </Container>
       </Navbar>
+
+  
     </div>
   )
 }
