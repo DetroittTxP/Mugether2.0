717fe10af34b1542482cab45c947c775576e12bf
Parame Srisomsap <s6304062616170@email.kmutnb.ac.th>
2023-10-01 16:47:29 +0700

    HEADER

--- mugether/src/components/Header.jsx
+++ mugether/src/components/Header.jsx
@@ -1,33 +1,41 @@
-import React,{useState} from 'react'
-import { Container, Nav, Navbar, Form} from 'react-bootstrap'
+import React, { useState } from 'react'
+import { Container, Nav, Navbar, Form } from 'react-bootstrap'
 import { Button, Dropdown } from 'antd';
 import Logo from '../assets/Pap.jpg'
 import { auto } from '@popperjs/core'
-import {SlLogin} from 'react-icons/sl'
-import {LuListFilter} from 'react-icons/lu'
-
+import { SlLogin } from 'react-icons/sl'
+import { LuListFilter } from 'react-icons/lu'
 
 
 export default function Header() {
+  const [text, Settext] = useState('')
 
   const items = [
     {
       key: '1',
       label: (
         <a href='#ken'>
-            <h7>LOGIN</h7>
+          <h7>LOGIN</h7>
         </a>
       ),
     },
     {
       key: '2',
       label: (
-         <a href='#beam'>
-            <h7>REGISTER</h7>
+        <a href='#beam'>
+          <h7>REGISTER</h7>
         </a>
       ),
     },
   ];
+
+  const onChange = (e) => {
+    Settext(e.target.value)
+  }
+
+  const onSearch = (search) => {
+    //api
+  }
 
 
   return (
@@ -35,28 +43,30 @@ export default function Header() {
       <Navbar style={{ padding: 30 }} bg="light" data-bs-theme="light">
         <Container>
           <Navbar.Brand href="#home">
-            <img src={Logo} height={80} width={100} style={{borderRadius:50}} />
+            <img src={Logo} height={80} width={100} style={{ borderRadius: 50 }} />
           </Navbar.Brand>
 
+          <Dropdown menu={{ items, }}>
+            <Nav style={{ margin: auto }} className="me-auto">
+              <Form.Control onChange={onChange} type="text" placeholder="Search here" style={{ width: 600 }} />
+            </Nav>
 
-          <Nav style={{ margin: auto }} className="me-auto">
-            <Form.Control type="text" placeholder="Search here" style={{ width: 600 }} />
-          </Nav>
+          </Dropdown>
 
-                 <Dropdown menu={{items,}} >
-                      <Button style={{width:95,height:50,textAlign:'center',borderRadius:50}}>
-                             <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                               <LuListFilter style={{fontSize:'larger'}} />
-                               <SlLogin style={{fontSize:'larger'}}/>
-                             </div>
-                      </Button>
-                 </Dropdown>
-    
+          <Dropdown menu={{ items, }} >
+            <Button style={{ width: 95, height: 50, textAlign: 'center', borderRadius: 50 }}>
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <LuListFilter style={{ fontSize: 'larger' }} />
+                <SlLogin style={{ fontSize: 'larger' }} />
+              </div>
+            </Button>
+          </Dropdown>
+
 
         </Container>
       </Navbar>
 
-  
+
     </div>
   )
 }
