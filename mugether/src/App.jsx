c8fa07f18aca39865e3304ff201adfe386dede4a
Parame Srisomsap <s6304062616170@email.kmutnb.ac.th>
2023-10-01 20:40:47 +0700

    NAVBAR 2

--- mugether/src/App.jsx
+++ mugether/src/App.jsx
@@ -7,6 +7,7 @@ export default function App() {
   return (
     <>
        <Header/>
+       <br/>
        
        <NavType/>
     </>
--- mugether/src/components/NavType.jsx
+++ mugether/src/components/NavType.jsx
@@ -1,12 +1,50 @@
 
-
-import React from 'react'
+import React from 'react';
+import { Nav, Container } from 'react-bootstrap';
 
 export default function NavType() {
-  return (
-    <div className='Nav-Type'>
-        test
-    </div>
-  )
-}
 
+  const type = [
+    {
+      type:'การเงิน'
+    },
+    {
+      type:'การงาน'
+    },
+    {
+      type:'ความรัก'
+    },
+    {
+      type:'โชคลาภ'
+    },
+    {
+      type:'สุขภาพ'
+    },
+    {
+      type:'การเรียน'
+    },
+    {
+      type:'การเดินทาง'
+    },
+  ]
+
+
+
+  return (
+    <Nav 
+        className='justify-content-center' 
+        variant="underline" 
+        onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+    
+    
+      >
+      {type.map(data => (
+        <Nav.Item style={{ marginRight: '50px' }}>
+            <Nav.Link eventKey={data.type} style={{color:'black'}} >{data.type}</Nav.Link>
+        </Nav.Item>
+      ))}
+    </Nav>
+
+
+  );
+}
