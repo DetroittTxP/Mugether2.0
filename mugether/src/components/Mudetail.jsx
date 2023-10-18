3dd186ff9cf075f3d9a368008604f134787f4f1d
Pattarakorn <119297049+bankptrk@users.noreply.github.com>
2023-10-18 19:40:20 +0700

    new new new

--- mugether/src/components/Mudetail.jsx
+++ mugether/src/components/Mudetail.jsx
@@ -1,94 +1,60 @@
-import React, { useEffect, useState, useContext } from 'react'
-import { Muplace_Context } from '../context/MuContext'
-import { Container, Row, Col } from 'react-bootstrap'
-import Reviewpage from './Reviewpage'
-
+import React, { useEffect, useState, useContext } from 'react';
+import { Muplace_Context } from '../context/MuContext';
+import { Container, Row, Col, Image, Button, Badge } from 'react-bootstrap';
+import Reviewpage from './Reviewpage';
+import './Mudetail.css'; 
 
 export default function Mudetail() {
+  const [Muplace, Setmuplace] = useState(localStorage.getItem('muplace'));
+  const { per_muplace } = useContext(Muplace_Context);
 
-  const [Muplace, Setmuplace] = useState(localStorage.getItem('muplace'))
-  const { per_muplace } = useContext(Muplace_Context)
   useEffect(() => {
     if (per_muplace !== "") {
-      localStorage.setItem('muplace', per_muplace)
-      Setmuplace(per_muplace)
+      localStorage.setItem('muplace', per_muplace);
+      Setmuplace(per_muplace);
+    } else {
+      Setmuplace(localStorage.getItem('muplace'));
     }
-    else {
-      Setmuplace(localStorage.getItem('muplace'))
-    }
-
-  }, [per_muplace])
-
+  }, [per_muplace]);
 
   return (
-    <div>
-     <div style={{ paddingTop: '0.5px', paddingLeft: '1px' }}>
-     <div style={{ display: 'flex', alignItems: 'center', marginLeft: '200px', justifyContent: 'space-between'}}>
-        <h1>{Muplace}</h1>
-        <div style={{ display: 'flex', alignItems: 'center', marginRight: '300px' }}>
-          <FaHeart style={{ marginRight: '10px', color: 'red', cursor: 'pointer' }} onClick={handleSave} />
-          <span>Save</span>
-          <FacebookShareButton url={window.location.href} quote={`Check out this place: ${Muplace}`}>
-            <FacebookIcon size={32} round={true} />
-          </FacebookShareButton>
-          <span style={{ marginRight: '10px' }}>Share</span>
-        </div>
-      </div>
-
-      <br />
-
-
-
-      <div style={{ display: 'flex', justifyContent: 'flex-start', marginLeft: 200 }}>
-        <img width={600} height={600}
-          src={`http://localhost:5353/image/mu/${Muplace}/1`}
-          style={{ marginRight: '10px', marginBottom: '10px', borderTopLeftRadius: '20px', borderBottomLeftRadius: '20px' }}/>
-
-        <div style={{ marginLeft: 30 }}>
-          <img width={300} height={300} src={`http://localhost:5353/image/mu/${Muplace}/2`} />
-          <img style={{ marginLeft: 30, borderTopRightRadius: '20px' }}  width={300} height={300} src={`http://localhost:5353/image/mu/${Muplace}/3`} /><br />
-          <img style={{ paddingTop: 30 }} width={300} height={300} src={`http://localhost:5353/image/mu/${Muplace}/4`} />
-          <img style={{ paddingTop: 30, paddingLeft: 30, borderBottomRightRadius: '20px' }} width={330} height={300} src={`http://localhost:5353/image/mu/${Muplace}/5`} />
-        </div>
-<<<<<<< HEAD
-=======
-
-
-        <br />
-       
-        <br />
-        <hr/>
-        <br/>
-        <Reviewpage Muplace_name={Muplace} />
-        <br/>
-        <br/>
-        
-
-
-
->>>>>>> 518e819aef875d069b0753a8cee84e7d4f831043
-      </div>
-      
-
-
-
-      <br />
-      <hr />
-      <br />
-
-      
-
-      <Reviewpage Muplace_name={Muplace} />
-
-
-      
-      
-
-
-
-
-    </div>
-      
-    </div>
-  )
+    <Container>
+      <Row>
+        <Col md={12} className="header">
+          <h1>{Muplace}</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={4} className="main-image">
+          <Image src={`http://localhost:5353/image/mu/${Muplace}/1`} alt="Main Image" className="big-image" fluid />
+        </Col>
+        <Col md={3} className="image-list">
+          <Row>
+            <Col md={12}>
+              <Image src={`http://localhost:5353/image/mu/${Muplace}/2`} alt="Image 2" className="small-image" fluid />
+            </Col>
+            <Col md={12}>
+              <Image src={`http://localhost:5353/image/mu/${Muplace}/3`} alt="Image 3" className="small-image" fluid />
+            </Col>
+          </Row>
+        </Col>
+        <Col md={3} className="image-list">
+          <Row>
+            <Col md={12}>
+              <Image src={`http://localhost:5353/image/mu/${Muplace}/4`} alt="Image 4" className="small-image" fluid />
+            </Col>
+            <Col md={12}>
+              <Image src={`http://localhost:5353/image/mu/${Muplace}/5`} alt="Image 5" className="small-image" fluid />
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={11} className="review-section">
+          <h2>Reviews</h2>
+          <Reviewpage Muplace_name={Muplace} />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
--- mugether/src/components/Reviewpage.jsx
+++ mugether/src/components/Reviewpage.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import ReactStars from 'react-rating-stars-component';
 import Rating from '@mui/material/Rating';
 
-import './ReviewPage.css';
+import './ReviewPage.css'; 
 
 export default function ReviewPage({ Muplace_name }) {
   const [detail, Setdetail] = useState([]);
@@ -17,25 +17,6 @@ export default function ReviewPage({ Muplace_name }) {
   }, []);
 
   return (
-<<<<<<< HEAD
-    <div style={{ marginLeft: 200 }}>
-         <h2>{detail.length} reviews</h2>
-
-         {detail.map((data,index) => {
-                return (
-                    <div>
-                         <img width={50} src='https://cdn.discordapp.com/attachments/1130047272508465273/1164158784046911498/image.png?ex=6542325b&is=652fbd5b&hm=34d3ee5ae415d18976b94fca7e67358183624112e20a65bfbfcb679cc5cede42&'/>
-                         <h4>{data.username}</h4>
-                         
-                         <Rating readOnly name='read-only' value={data.score}/>
-                         <h5>{data.detail}</h5>
-                          
-                         <hr />
-                    </div>
-                )
-         })}
-         
-=======
     <div className="review-container">
       <h2 className="review-title">{detail.length} Reviews</h2>
 
@@ -51,7 +32,6 @@ export default function ReviewPage({ Muplace_name }) {
           </div>
         );
       })}
->>>>>>> 518e819aef875d069b0753a8cee84e7d4f831043
     </div>
   );
 }
