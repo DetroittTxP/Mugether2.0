484779739ef11d7b8e1e0e242236cd0be136d9f9
Parame Srisomsap <s6304062616170@email.kmutnb.ac.th>
2023-10-18 15:42:04 +0700

    Update API FOR NEARBY PLACE

--- server/routers/Mu_place.js
+++ server/routers/Mu_place.js
@@ -35,6 +35,93 @@ mu.put('/addreviewmuplace',(req,res) => {
 })
 
 
+mu.get('/nearby/travel/:muplace_name',async (req,res) => {
+
+       const {muplace_name} = req.params;
+
+       let result = await client.db(process.env.DATABASE)
+                    .collection(process.env.TRAVEL)
+                    .find({mu_place:{$regex:muplace_name}},{projection:{"photo_path":0}})
+                    .toArray()
+       await client.close();
+       
+       res.json(result)
+        
+})
+
+mu.get('/nearby/food/:muplace_name',async (req,res) => {
+
+       const {muplace_name} = req.params;
+
+       let result = await client.db(process.env.DATABASE)
+                    .collection(process.env.FOOD)
+                    .find({mu_place:{$regex:muplace_name}},{projection:{"photo_path":0}})
+                    .toArray()
+       await client.close();
+       
+       res.json(result)
+                    
+        
+})
+
+mu.get('/nearby/hotel/:muplace_name',async (req,res) => {
+
+       const {muplace_name} = req.params;
+
+      let result = await client.db(process.env.DATABASE)
+                    .collection(process.env.HOTEL)
+                    .find({mu_place:{$regex:muplace_name}},{projection:{"photo_path":0}})
+                    .toArray()
+       await client.close();
+       
+       res.json(result)
+       
+       res.json(result)
+        
+})
+
+
+
+
+mu.get('/nearby/multiple/:muplace_name',async (req,res) => {
+
+       const {muplace_name} = req.params;
+
+       let resultHOTEL = await client.db(process.env.DATABASE)
+                    .collection(process.env.HOTEL)
+                    .find({mu_place:{$regex:muplace_name}},{projection:{"photo_path":0}})
+                    .toArray()
+     
+
+       let resultFOOD = await client.db(process.env.DATABASE)
+                    .collection(process.env.FOOD)
+                    .find({mu_place:{$regex:muplace_name}},{projection:{"photo_path":0}})
+                    .toArray()
+
+
+       let resultTRAVEL = await client.db(process.env.DATABASE)
+                    .collection(process.env.TRAVEL)
+                    .find({mu_place:{$regex:muplace_name}},{projection:{"photo_path":0}})
+                    .toArray()
+       await client.close();
+
+      return res.json({
+              status:'ok',
+              travel:resultTRAVEL,
+              food:resultFOOD,
+              hotel:resultTRAVEL
+       })
+
+
+})
+
+
+
+
+
+
+
+
 
 mu.get('/',(req,res) => {
        res.send('MU PLACE OK')
