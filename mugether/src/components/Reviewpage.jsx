d2bfcaedef269b468594cac84a45dba75a9a17bf
Pattarakorn <119297049+bankptrk@users.noreply.github.com>
2023-10-18 18:26:14 +0700

    review css

--- mugether/src/components/Reviewpage.jsx
+++ mugether/src/components/Reviewpage.jsx
@@ -1,39 +1,37 @@
-import React,{useState,useEffect} from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import ReactStars from 'react-rating-stars-component';
 import Rating from '@mui/material/Rating';
-export default function Reviewpage({Muplace_name}) {
-  
-  const [detail,Setdetail] = useState([]);
-  const [rating,Setrating] = useState(0);
 
+import './ReviewPage.css';
+
+export default function ReviewPage({ Muplace_name }) {
+  const [detail, Setdetail] = useState([]);
 
   useEffect(() => {
     axios.get(`http://localhost:5353/muplace/mudata/${Muplace_name}`)
-    .then(res => {
-        Setdetail(res.data[0].review)
-        
-    })
-    .catch(err => alert(err))
-  },[])
+      .then((res) => {
+        Setdetail(res.data[0].review);
+      })
+      .catch((err) => alert(err));
+  }, []);
 
   return (
-    <div>
-         <h2>{detail.length} reviews</h2>
+    <div className="review-container">
+      <h2 className="review-title">{detail.length} Reviews</h2>
 
-         {detail.map((data,index) => {
-                return (
-                    <div>
-                         <img width={50} src='https://cdn.discordapp.com/attachments/1130047272508465273/1164158784046911498/image.png?ex=6542325b&is=652fbd5b&hm=34d3ee5ae415d18976b94fca7e67358183624112e20a65bfbfcb679cc5cede42&'/>
-                         <h4>{data.username}</h4>
-                         
-                         <Rating readOnly name='read-only' value={data.score}/>
-                         <h5>{data.detail}</h5>
-                          
-                         <hr/>
-                    </div>
-                )
-         })}
+      {detail.map((data, index) => {
+        return (
+          <div key={index} className="review-item">
+            <img className="avatar" src='https://media.discordapp.net/attachments/1130047272508465273/1164158784046911498/image.png?ex=6542325b&is=652fbd5b&hm=34d3ee5ae415d18976b94fca7e67358183624112e20a65bfbfcb679cc5cede42&=&width=445&height=385' alt={data.username} />
+            <div className="review-content">
+              <h4 className="username">{data.username}</h4>
+              <Rating className="rating" readOnly name='read-only' value={data.score} />
+              <p className="review-text">{data.detail}</p>
+            </div>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
