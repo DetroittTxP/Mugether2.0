b6a3e031eab4471d9a7b660b542d4cf82fdb5a40
Parame Srisomsap <s6304062616170@email.kmutnb.ac.th>
2023-10-19 17:41:49 +0700

    TRY TO CREAT LOGIN

--- server/routers/User.js
+++ server/routers/User.js
@@ -1,21 +1,33 @@
 const User = require('express').Router();
 const {MongoClient} = require('mongodb')
 require('dotenv').config({path:'../.env'})
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
+
+const client = new MongoClient(process.env.CONNECT_STRING_POND);
 
 
 User.get('/',(req,res) => {
     res.send('ok ken ok ken oken ')
 })
 
-
-User.post('/register',(req,res) => {
+//login
+User.post('/login',(req,res) => {
     res.send('ok ken ok ken oken ')
 })
 
+//regsiter
+User.post('/regsiter',async(req,res) => {
+    const {Username,password,email} = req.body;
 
-User.post('/login',(req,res) => {
-    res.send('ok ken ok ken oken ')
+    bcrypt.hash(password,10,(err,hash) => {
+         if(err){
+            res.send('error');
+         }
+
+         
+    })
 })
 
 
