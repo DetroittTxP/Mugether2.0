040e36bac932934ac4a1a718f9bbc0b3ad20fa01
Parame Srisomsap <s6304062616170@email.kmutnb.ac.th>
2024-01-10 15:11:02 +0700

    upload guide profile image

--- server-2/routers/uploadimages.js
+++ server-2/routers/uploadimages.js
@@ -3,11 +3,12 @@ const path = require('path');
 const multer = require('multer')
 const upload_img = require('express').Router();
 const user_db = require('../model/User-model');
+const guide_db = require('../model/Guide-Model');
 
 //create dir to store image file
-const create_dir=async(username,type)=>{
+const create_dir=async(username,type,uploadtype)=>{
     let dir = path.dirname(__dirname);
-    let dir_name =path.join(dir,"assets",type,username,"profile_pic");
+    let dir_name =path.join(dir,"assets",type,username,uploadtype);
     if(!fs.existsSync(dir_name))
     {
         fs.mkdirSync(dir_name,{recursive:true});
@@ -20,7 +21,7 @@ const create_dir=async(username,type)=>{
 const userprofile = multer.diskStorage({
     destination:async(req,file,cb) => {
         const {username} = req.params;
-        let dir_name = await create_dir(username,"user");
+        let dir_name = await create_dir(username,"user","profile_pic");
         cb(null,dir_name);
     }, 
     filename:(req,file,cb)=>{
@@ -57,6 +58,53 @@ upload_img.post('/user/profile/:username',upload_user_profile.single('profile_im
 
 
 
+//handle guide upload profile image
+const guideprofile = multer.diskStorage({
+    destination:async(req,file,cb) => {
+          const {username} = req.params;
+          let dir_name = await create_dir(username,"guide","profile_pic");
+          cb(null,dir_name)
+    },
+    filename:(req,file,cb)=>{
+        cb(null,Date.now() +  req.params.username + file.originalname);
+    }
+})
+
+const upload_guide_profile = multer({storage:guideprofile});
+
+upload_img.post('/guide/profile/:username',upload_guide_profile.single('profile_img'),
+    async(req,res) => {
+        const {username} = req.params;
+        try{
+             let update = await guide_db.updateOne(
+                {username:username},
+                {
+                    $set:{
+                        profile_pic:req.file.filename
+                    }
+                    }
+             )
+             res.send({
+                status:'success',
+                update,
+            })
+        }
+        catch(err)
+        {
+         res.send(err);
+         
+        }
+
+})
+
+
+
+//upload mutiple image for guide
+
+
+
+
+
 
 
 
@@ -65,8 +113,6 @@ upload_img.post('/user/profile/:username',upload_user_profile.single('profile_im
 upload_img.get('/',(req,res) =>{
     res.send('ok kub upload')
 })
-
-
 
 
 
