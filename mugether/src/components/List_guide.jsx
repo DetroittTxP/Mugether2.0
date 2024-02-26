e89f703c4085ade51f8a4d9e0b247b310ce3c6c6
Krittin Promsirinimit <97147567+XzeCute45@users.noreply.github.com>
2024-02-26 18:17:41 +0700

    Page for add picture for guide

--- mugether/src/components/List_guide.jsx
+++ mugether/src/components/List_guide.jsx
@@ -41,14 +41,16 @@
 
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Container, Row, Col, Button,Image } from 'react-bootstrap';
-import './ReviewPage.css';
+import { Container, Row, Col, Button,Image, Modal, Form} from 'react-bootstrap';
+import './List_guide.css';
 import Guide_detail from './Guide_detail';
 
 import { Accordion, AccordionDetails,  AccordionSummary } from '@mui/material'
 
 export default function ListGuide  ()  {
     const [guidedata, setGuideData] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+    const [selectedFiles, setSelectedFiles] = useState([]);
 
     useEffect(() => {
         axios.get(`http://localhost:5353/guide/list-guide/${localStorage.getItem('muplace')}`)
@@ -56,38 +58,83 @@ export default function ListGuide  ()  {
             .catch(err => alert(err));
     }, []);
 
+    const SelectPicture = (event) => {
+        if (event.target.files.length > 5) {
+          alert("You can only upload a maximum of 5 images.");
+          event.target.value = null; // Reset the file input
+        } else {
+          setSelectedFiles(event.target.files);
+        }
+      };
+
 
     return (
         <div>
-            <h1>CHOOSE YOUR GUIDE</h1><br/>
-            {guidedata.map((data) => (
-                <Accordion onChange={() => Setusername_guide(data.username)}>
-                    <AccordionSummary
-                        aria-controls="panel1-content"
-                        id="panel1-header"
-                    >
-                        <Image 
-                              src={`http://localhost:5353/image/guide/profile/${data.username}/${data.profile_pic}`} 
-                              roundedCircle
-                              className='avatar'
-                              />   
-                                        
-                         <span style={{marginLeft:10}}>
-                            <b>นาย {data.firstname} {data.lastname}</b>
-                        </span>
-                    </AccordionSummary>
+            <Container>
+                <Row>
+                    <Col>
+                        <h1>CHOOSE YOUR GUIDE</h1>
+                    </Col>
+                    <Col>
+                    <Button variant='primary' className='add-post' onClick={() => setShowModal(true)}>
+                        ADD POST
+                    </Button>
+                    </Col>
+                </Row>
+                <br/>
 
-                    <AccordionDetails>
-                    <Guide_detail username={data.username} />
-                        </AccordionDetails>
-                </Accordion>
-            ))}
+                <Modal show={showModal} onHide={() => setShowModal(false)}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Add Post</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form>
+                            <Form.Group controlId="postContent">
+                                <Form.Label>รายละเอียดกิจกรรม</Form.Label>
+                                <Form.Control as="textarea" rows={3} />
+                            </Form.Group>
+                            <Form.Group controlId="formFile" className="mb-3">
+                                <Form.Label>เพิ่มรูปภาพของคุณ (ไม่เกิน 5 รูป)</Form.Label>
+                                <Form.Control type="file" multiple onChange={SelectPicture} />
+                            </Form.Group>
+                        </Form>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={() => setShowModal(false)}>Close</Button>
+                        <Button variant="primary">Post</Button>
+                    </Modal.Footer>
+                </Modal>
+
+                {guidedata.map((data) => (
+                    <Accordion onChange={() => Setusername_guide(data.username)}>
+                        <AccordionSummary
+                            aria-controls="panel1-content"
+                            id="panel1-header"
+                        >
+                            <Image 
+                                src={`http://localhost:5353/image/guide/profile/${data.username}/${data.profile_pic}`} 
+                                roundedCircle
+                                className='avatar'
+                                />   
+                                            
+                            <span style={{marginLeft:10}}>
+                                <b>นาย {data.firstname} {data.lastname}</b>
+                            </span>
+                        </AccordionSummary>
+
+                        <AccordionDetails>
+                        <Guide_detail username={data.username} />
+                            </AccordionDetails>
+                    </Accordion>
+                ))}
+            </Container>
+            
         </div>
 
 
     )
 }
-
+ 
 
 // export default function ListGuide() {
 //     const [guidedata, setGuideData] = useState([]);
