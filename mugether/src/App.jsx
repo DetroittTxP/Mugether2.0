cb16e4bd5cf676eabce30493c48de7a2687dcfb2
Peerawut Keawnoi <109844635+kenpeerawut@users.noreply.github.com>
2024-04-08 23:31:55 +0700

    up env render

--- mugether/src/App.jsx
+++ mugether/src/App.jsx
@@ -55,6 +55,7 @@ const Checktimeout=(timeout,onLogout)=>{
 
 export default function App() {
   const SERVER_URL = import.meta.env.VITE_SERVER_URL
+  
   const location = useLocation();
   const [global_muplace, Setmuplace] = useState([]);
   const [global_shop, Setshop] = useState([]);
