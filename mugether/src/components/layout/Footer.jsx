4112d88408be2b75fd6a6b1bc0a9b2db70014cc5
Peerawut Keawnoi <109844635+kenpeerawut@users.noreply.github.com>
2024-04-09 14:49:29 +0700

    footer v2

--- mugether/src/components/layout/Footer.jsx
+++ mugether/src/components/layout/Footer.jsx
@@ -12,14 +12,14 @@ function Footer() {
                         <h5>เกี่ยวกับเรา </h5>
                         <p>Mugether คือเว็บไซต์ที่เน้นการช่วยให้ผู้เริ่มต้นในการผจญภัยของสายมูได้ใช้หาข้อมูลอย่างสะดวกสบาย และให้ข้อมูลที่ครอบคลุมเกี่ยวกับสถานที่และเนื้อหาที่เกี่ยวข้องกับสายมูทั้งหมด เพื่อให้ประสบการณ์การเดินทางของพวกเขาเป็นประสบการณ์ที่น่าจดจำและเต็มไปด้วยความรู้สึกดีตลอดกาล.</p>
                     </div>
-                    {/* <div className="col-md-6">
-                        <h5>Contact Us</h5>
+                    <div className="Text col-md-6">
+                        <h5>ติดต่อเรา</h5>
                         <ul className="list-unstyled">
-                            <li>Email: info@example.com</li>
-                            <li>Phone: +123-456-7890</li>
-                            <li>Address: 123 Street, City, Country</li>
+                            <li>Email:  Mugether_Official@gmail.com</li>
+                            <li>Phone:  08x-xxx-xxxx</li>
+                            <li>Address:  มหาวิทยาลัยเทคโนโลยีพระจอมเกล้าพระนครเหนือ</li>
                         </ul>
-                    </div> */}
+                    </div>
                 </div>
             </div>
             <div className="footer-bottom">
