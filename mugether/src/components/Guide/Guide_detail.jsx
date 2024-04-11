1c0de133ebc6d3860bb2ee8c048b1f4a873c9ee2
Peerawut Keawnoi <109844635+kenpeerawut@users.noreply.github.com>
2024-04-11 13:50:20 +0700

    up pic

--- mugether/src/components/Guide/Guide_detail.jsx
+++ mugether/src/components/Guide/Guide_detail.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState,useContext } from 'react'
-import { Row, Col, Container, Image,Carousel } from 'react-bootstrap'
+import React, { useEffect, useState, useContext } from 'react'
+import { Row, Col, Container, Image, Carousel } from 'react-bootstrap'
 import './Guide_detail.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Muplace_Context } from '../../context/MuContext';
@@ -10,33 +10,34 @@ export default function Guide_detail({ profile_name, contact, data }) {
 
 
   const muplace = localStorage.getItem('muplace');
-  const [newdata,Setnewdata] = useState(data);
-  const {SERVER_URL} = useContext(Muplace_Context)
-  const [contactt,Setcontactt] = useState({
-     tel:'',
-     email:''
+  const [newdata, Setnewdata] = useState(data);
+  const { SERVER_URL } = useContext(Muplace_Context)
+  const [contactt, Setcontactt] = useState({
+      tel: '',
+      email: ''
   })
+  
   useEffect(() => {
 
     let newdata = {
-          firstname:data.firstname,
-          lastname:data.lastname,
-          guide_post:data.guide_post.filter((e) => e.muplace === muplace)
+      firstname: data.firstname,
+      lastname: data.lastname,
+      guide_post: data.guide_post.filter((e) => e.muplace === muplace)
     }
     Setnewdata(newdata);
 
     Setcontactt(contact)
-  },[])
+  }, [])
 
-  
+
   return (
     <div>
       <h2><b>{data.firstname} {data.lastname}</b></h2> <br />
-      
+
       <Container>
 
 
-        
+
 
         <Row>
           <Col md={5} className="main-image">
@@ -49,7 +50,7 @@ export default function Guide_detail({ profile_name, contact, data }) {
             />
 
           </Col>
-           <Col md={3} className="image-list">
+          <Col md={3} className="image-list">
             <Row>
               <Col md={12}>
                 <Image
@@ -59,7 +60,7 @@ export default function Guide_detail({ profile_name, contact, data }) {
                   fluid
                 />
               </Col>
-              
+
               <Col md={12}>
                 <Image
                   src={`${SERVER_URL}/image/guide/detail/${data.id_guide}/${newdata.guide_post[0].postPhotos[2]}`}
@@ -88,9 +89,9 @@ export default function Guide_detail({ profile_name, contact, data }) {
                   fluid
                 />
               </Col>
-            
+
             </Row>
-          </Col> 
+          </Col>
         </Row>
       </Container>
 
@@ -100,22 +101,22 @@ export default function Guide_detail({ profile_name, contact, data }) {
         <h5>{newdata.guide_post[0].postDetail}</h5>
         <br />
         <h2><b>รายละเอียดกิจกรรม</b></h2>
-        <br/>
+        <br />
         <ul>
 
-      
-        {newdata.guide_post[0].postActivity.map((data,i) =>{
+
+          {newdata.guide_post[0].postActivity.map((data, i) => {
             return (
-                <li>
-                    <h5>{data}</h5>
-                </li>
+              <li>
+                <h5>{data}</h5>
+              </li>
             )
-        })}
+          })}
         </ul>
-        <br/>
-        <h2><b>ประสบการณ์ (Experience)</b></h2>    <br/>
-        <Col md={{span: 6, offset: 3}} >
-          <Carousel className='picture-guidedetail'indicators controls>
+        <br />
+        <h2><b>ประสบการณ์ (Experience)</b></h2>    <br />
+        <Col md={{ span: 6, offset: 3 }} >
+          <Carousel className='picture-guidedetail' indicators controls>
             {newdata.guide_post[0].experience_img.map((image, index) => (
               <Carousel.Item key={index}>
                 <Image
@@ -123,27 +124,27 @@ export default function Guide_detail({ profile_name, contact, data }) {
                   rounded
                   src={`${SERVER_URL}/guide_detail/exp/img/${data.id_guide}/${image}`}
                   alt={`Product image ${index + 1}`}
-                  style={{width: '100%'}}
+                  style={{ width: '100%' }}
                 />
               </Carousel.Item>
             ))}
           </Carousel>
         </Col>
-        
-        <br/>    <br/>
+
+        <br />    <br />
         <h2><b>ติดต่อ</b></h2>
-          <h3>เบอร์โทร: {contactt.tel} </h3>
-          <h3>อีเมลล์: {contactt.email} </h3>
+        <h3>เบอร์โทร: {contactt.tel} </h3>
+        <h3>อีเมลล์: {contactt.email} </h3>
 
-        <br/>
+        <br />
 
-                <h2><b>รีวิว</b></h2> 
-                  
-                  <ReviewGuide profile_name={profile_name} postID={newdata.guide_post[0]._id} reviewdata2={newdata.guide_post[0].postReview} guideID={data.id_guide} reviewdata={data.guide_review}/>
-       
+        <h2><b>รีวิว</b></h2>
+
+        <ReviewGuide profile_name={profile_name} postID={newdata.guide_post[0]._id} reviewdata2={newdata.guide_post[0].postReview} guideID={data.id_guide} reviewdata={data.guide_review} />
+
 
       </div>
-           
+
 
     </div>
   )
