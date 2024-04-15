a8c1a2bb6222afa25784b176fed02e023b20da16
Parame Srisomsap <s6304062616170@email.kmutnb.ac.th>
2024-04-15 14:54:18 +0700

    make muplace can view closesr by clicked

--- mugether/src/components/muplace/Mudetail.jsx
+++ mugether/src/components/muplace/Mudetail.jsx
@@ -10,32 +10,37 @@ import Map from "../util/Map";
 import { Accordion, AccordionSummary, AccordionDetails } from '@mui/material'
 import { ArrowDropDown } from '@mui/icons-material'
 import ReactPlayer from 'react-player'
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 
 export default function Mudetail({ showguide }) {
   const [Muplace, Setmuplace] = useState(localStorage.getItem("muplace"));
-  const { per_muplace, muplace,SERVER_URL } = useContext(Muplace_Context);
+  const { per_muplace, muplace, SERVER_URL } = useContext(Muplace_Context);
   const [mudetail, Setmudetail] = useState(() => {
     const storedDetail = localStorage.getItem("mudetail");
     return storedDetail !== null ? storedDetail : "Mai me data";
   });
-  const [video_url,Setvidourl] = useState('#')
- 
+  const [video_url, Setvidourl] = useState('#')
+  const [open, setOpen] = useState(false);
+  const [imageinfo, Setimageinfo] = useState([{ key: '', name: '' }])
+  const [index, setIndex] = useState(0);
+  const updateIndex = ({ index: current }) => setIndex(current);
 
- 
-  useEffect(() =>{
-    if(muplace.length !== 0){
+
+  useEffect(() => {
+    if (muplace.length !== 0) {
       let detail = muplace.filter(data => data.name === Muplace);
       if (detail[0].place_detail !== undefined) {
         Setmudetail(detail[0].place_detail);
         localStorage.setItem("mudetail", detail[0].place_detail);
         Setvidourl(detail[0].video_url)
 
-      }else{
-        Setmudetail("Mai me data"); 
+      } else {
+        Setmudetail("Mai me data");
         localStorage.setItem("mudetail", "Mai me data");
       }
     }
-  },[Muplace, muplace,localStorage.getItem('muplace')]);
+  }, [Muplace, muplace, localStorage.getItem('muplace')]);
 
   useEffect(() => {
     if (per_muplace !== "") {
@@ -44,18 +49,37 @@ export default function Mudetail({ showguide }) {
     } else {
       Setmuplace(localStorage.getItem("muplace"));
     }
-  }, [per_muplace,localStorage.getItem('muplace')]);
+  }, [per_muplace, localStorage.getItem('muplace')]);
 
   const pageUrl = window.location.href;
 
   return (
     <Container>
+
+      <Lightbox
+        open={open}
+        close={() => setOpen(false)}
+        slides={[
+          { src:`${SERVER_URL}/image/mu/${Muplace}/1`},
+          { src:`${SERVER_URL}/image/mu/${Muplace}/2`},
+          { src: `${SERVER_URL}/image/mu/${Muplace}/3` },
+          { src:`${SERVER_URL}/image/mu/${Muplace}/4` },
+          { src:`${SERVER_URL}/image/mu/${Muplace}/5` },
+          { src:`${SERVER_URL}/image/mu/${Muplace}/6` },
+          { src:`${SERVER_URL}/image/mu/${Muplace}/7` },
+          { src:`${SERVER_URL}/image/mu/${Muplace}/8` }
+        ]}
+        index={index}
+        on={{ view: updateIndex }}
+      />
+
+      
       <Row className="header-row">
         <Col className="header-title">
           <h1><b>{Muplace}</b></h1>
         </Col>
         <Col xs="auto" className="share-button-col">
-          
+
           <ShareButton url={pageUrl} />
         </Col>
       </Row>
@@ -63,6 +87,7 @@ export default function Mudetail({ showguide }) {
       <Row>
         <Col md={5} className="main-image">
           <Image
+            onClick={() =>  setOpen(true)}
             src={`${SERVER_URL}/image/mu/${Muplace}/1`}
             alt="Main Image"
             className="big-image"
@@ -105,6 +130,7 @@ export default function Mudetail({ showguide }) {
                 alt="Image 5"
                 className="small-image figure-4"
                 fluid
+                style={{ cursor: 'zoom-in' }}
               />
             </Col>
           </Row>
@@ -122,20 +148,20 @@ export default function Mudetail({ showguide }) {
         >
           <h2><b>ประวัติความเป็นมา</b></h2>
         </AccordionSummary>
-     
+
         <AccordionDetails>
           <div className="detail" style={{ zIndex: 0 }}>
             <h5>{mudetail}</h5>
           </div>
-          
-          <br/>
+
+          <br />
           <h2><b>วิดีโอของสถานที่มู</b></h2>
           <div className="Video">
-            <br/>
-            
-             { video_url !== '#' &&  <ReactPlayer url={(video_url || '#')} style={{ maxWidth: '100%', height: 'auto' }} />}
+            <br />
+
+            {video_url !== '#' && <ReactPlayer url={(video_url || '#')} style={{ maxWidth: '100%', height: 'auto' }} />}
           </div>
-          <br/>
+          <br />
         </AccordionDetails>
       </Accordion>}
 
@@ -153,7 +179,7 @@ export default function Mudetail({ showguide }) {
       </Row>
 
       <br />
-     
+
       <Row>
         <Col md={11} className="review-section">
           {!showguide && <Nearby Muplace_name={Muplace} />}
